f86f9277c6fc8e2ec9f24afa07a4b0258c2bfbe6
Denissimo1 <47557391+Denissimo1@users.noreply.github.com>
2024-10-24 17:05:55 +0300

    Add files via upload

--- MySolCV1.cpp
+++ MySolCV1.cpp
@@ -13,22 +13,31 @@
 //#include <GeographicLib/LocalCartesian.hpp>
 #include <filesystem>
 #include "common.h"
-#include "IP/SimpleNet.h"
+
+#include "IP/SimpleNetMy.h"
+
+//#include <PWNNLib2_0/SimpleNet.h>
+//#include <PWNGeneral/Dataset.h>
 
 //#include "SuperFilter.h"
 
 using namespace std;
-using namespace GeographicLib;
+//using namespace GeographicLib;
 namespace fs = std::filesystem;
+
+
+#define TEST
+
 
 int main()
 {
+#ifdef TEST
 	SimpleNet model;
-	model.load("net_data/model8-5.net");
-	std::vector <float> f, out, out_2;
+	model.load("data_features_8_to_5.net");
+	std::vector <float> f, out;
 	out.resize(model.getOutputsCount());
 	
-	//SuperVelFilter SVF(0.1);
+#else
 
 	/////////////////
 	vector<cv::Point2d> gps_traj;
@@ -45,35 +54,28 @@ int main()
 	vector<cv::Point3d> ang_vel;
 	vector<float> vh;
 	make_ang_v(pack_ts, ang_v, ang_vel, h, vh); 
+#endif
 	//save_traj_gps(gps_traj, "gps_meters.csv");
 	vector<vector<float> > features, features_mirr;
 
 	GetMatches get_matches("distors.txt", "intrincics.txt");
 	uint64_t ts = 0;
 
-	std::vector<Camera> cams;
-
 	int ii = 0;
 	int bad_fr = 0;
 
 	double all_tm = 0;
- 
-	//Зададим начальное положение
-	Camera drone;
-	drone.set_start_pos(360, 240);
-
 	double k = 0.05;
 	
 	static fs::directory_iterator di(".\\data\\frame2");
 	static auto it = begin(di);
-	//for (size_t i=0; i<250;i++)
+
 	cv::VideoWriter video("demo.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 25, cv::Size(720, 480*2));
 
 	//карта в реалтайм
 	cv::Mat field(480,720, CV_8UC3, cv::Scalar(0, 0, 0));
 	cv::circle(field, cv::Point2d(360, 240), 3, cv::Scalar(100, 100), 2);
 	cv::Point3d c1(360, 240, 0);
-
 
 	float cur_az = 0;
 	float cur_v = 0;
@@ -84,6 +86,11 @@ int main()
 	float v = 0;
 	float hh = 0;
 	float fvh = 0;
+	std::vector<float> fl;
+	fout << "wx" << ";" << "wy" << ";" << "wz" << ";" << "vh" << ";" << "v" << ";" << "pos_x" << ";" << "pos_y" << ";" << "h" << "\n";
+	cv::Point3d p;
+	float az_start = -0.45f;
+
 	while (true)
 	{
 		ii++;
@@ -106,44 +113,65 @@ int main()
 		crop_props(v1, v2);
 		draw_matches(cropped_image, v1, v2);
 	
+#ifdef TEST
 		//прямой проход Нейросети
 		if (v1.size() > 15)
 		{
 			matchFeaturing(v1, v2, f);
-			model.compute(&f[0], &out[0]);		
+			if (fl.size() != 0)
+			{
+				for (size_t j = 0; j < f.size(); j++)
+				{
+					if (f[j] == 0) {
+						f[j] = fl[j];//заполняем от предыдущих фич
+					}
+				}
+			}
+			fl = f;
+			model.compute(&f[0], &out[0]);				
 			az =out[2]; 
 			v = out[4];
 			hh = hh + out[3] * 0.025f;
-			const float af = 0.01f;
+			const float af = 0.02f;
 			fvh = fvh * (1 - af) + out[3] * af;
-			if (fabsf(fvh) > 0.05f) v = 0;		
+			//fout2 << out2[0] << ";" << out2[1] << ";" << out2[2] << "\n";
+			if (fabsf(fvh) > 0.5f) v = 0;		
+		}
+		else
+		{
+			f = { 0,0,0,0,0,0,0,0 };
 		}
 		
-		const float ka = 0.03f;
-		cur_az += az * ((float)(frame_ts[ii] - frame_ts[ii - 1])) / 1000000.f;//угол из угловой скорости
-		cv::Point3d p(c1);
+		const float ka = 0.025f;
+		const float kaj = 0.012f;
+		cur_az += az * 0.033f;//угол из угловой скорости
+		cv::Point3d pview(c1);
+		
 		const float a = 0.1f;
-		v_base = 6.f;
 		cur_v = cur_v * (1.f - a) + a * v;
 
-		p.x+=cos(cur_az)*cur_v*ka;
-		p.y+=sin(cur_az)*cur_v*ka;
+		pview.x+=cos(cur_az)*cur_v*ka;
+		pview.y+=sin(cur_az)*cur_v*ka;
+		p.x += cos(az_start + cur_az)*cur_v*kaj;
+		p.y += sin(az_start + cur_az)*cur_v*kaj;
 
 		const float scale = 3.f;
-		fout << out[0] << ";" << out[1] << ";" << out[2] << ";" << out[3] << ";" << out[4] << ";" << p.x*scale<< ";" << p.y*scale<< "\n";
+		fout << out[0] << ";" << out[1] << ";" << out[2] << ";" << fvh << ";" << out[4] << ";" << p.x*scale<< ";" << p.y*scale<< ";"<<hh<<"\n";
 
 		cv::Point c1i = static_cast<cv::Point2i>(cv::Point2d(c1.x, c1.y));
-		cv::Point c2i = static_cast<cv::Point2i>(cv::Point2d(p.x, p.y));
+		cv::Point c2i = static_cast<cv::Point2i>(cv::Point2d(pview.x, pview.y));
 		cv::line(field, c1i, c2i, cv::Scalar(100, 20, 220), 3);
-		c1 = p;
+		c1 = pview;
+#endif
 		///-----------------------------
 		cv::Mat res_img;
 		cv::vconcat(cropped_image, field, res_img);
 		cv::imshow("Trace", res_img);
 		cv::waitKey(1);
 		video.write(res_img);
+#ifndef TEST	
 		///data_grab
-		/*if (v1.size() > 15)
+		if (v1.size() > 15)
 		{			
 			vector<float> f , f_mirr;
 			matchFeaturing(v1, v2, f);
@@ -153,21 +181,21 @@ int main()
 		}
 		else
 		{
-			//vector<float> f{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
 			vector<float> f{ 0,0,0,0,0,0,0,0};
 			features.push_back(f);
 			features_mirr.push_back(f);
-		}*/
+		}
+#endif
 	}
 
+#ifndef TEST	
 	//сохранение нового файла с фичами
-	//save_synchronized_data("data_features2.csv", pack_ts, ang_vel, h, vh, gps_ts, vel, frame_ts, features, features_mirr);
-	printf("i = %d	bad_fr = %d  %f\n",ii, bad_fr, all_tm / 300.f);
+	save_synchronized_data("out/data_features_8_to_5.csv", pack_ts, ang_vel, h, vh, gps_ts, vel, frame_ts, features, features_mirr);
+#endif
+
 	fout2.close();
 	fout.close();
 	cv::waitKey(0);
 	//save_traj_csv(cams, "traj.csv");
-	///нарисуем 2д траекторию
-	//draw_2D_trajectoru(cams);
 	
 }
